30449208a6e8ad7c9285ce51e141eaca8a70aade
Daniel <dzayas79@gmail.com>
2024-08-21 22:09:10 -0400

    mapped out components needed based on excalidraw sketch

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Header from './components/Header';
 function App() {
   return (
     <>
+      {/* header
+    search (within database, later we will CRUD)
+    display (with child components most likely) */}
+
       <h1>building glossary</h1>
       <Header />
     </>
