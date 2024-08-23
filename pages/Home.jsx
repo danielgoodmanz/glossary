2f9a5b68b3263e499484f50ee459d86c6aa530be
Daniel Zayas <dan@Daniels-MBP-2.attlocal.net>
2024-08-23 09:55:26 -0400

    rearranging components

---
+++ pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from '../src/components/Header';
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>building glossary</h1>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/Header';
+
 
 function App() {
   return (
@@ -6,9 +6,6 @@ function App() {
       {/* header
     search (within database, later we will CRUD)
     display (with child components most likely) */}
-
-      <h1>building glossary</h1>
-      <Header />
     </>
   );
 }
