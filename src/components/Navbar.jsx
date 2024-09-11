fbf8ad897b2b6d4f6f13530ccafd72d1827661c3
Daniel <dzayas79@gmail.com>
2024-09-10 21:46:17 -0400

    set up basic routes with rrouterdom

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to="/">
+        <h1>gloss home</h1>
+      </Link>
+      <Link to="/add">
+        <h1>contribute</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
