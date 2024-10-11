8e5d2a3b830f49ef5b739d8a02c2a28eeaada0d6
Daniel <dzayas79@gmail.com>
2024-10-11 01:35:56 -0400

    setting up shadcn cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,43 @@
 import Home from '../pages/Home';
 import Navbar from './components/Navbar';
 
+//shadcn test
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 function App() {
   return (
-    <div className='font-bold bg-white text-red-400 min-h-screen'>
-      <Navbar />
-      <Home />
-      <footer className='flex justify-between text-xs'>
-        <p>by Dan</p>
-        <p>glossary v1</p>
-      </footer>
+    <div>
+      <div className="font-bold bg-neutral-900 text-red-400 min-h-screen">
+        <Navbar />
+        <Home />
+        <footer className="flex justify-between text-xs">
+          <p>by Dan</p>
+          <p>glossary v1</p>
+        </footer>
+      </div>
+      <div>
+        {/* sample card but lets keep simple & build up, perhaps a button that opens
+        the card in a new route /:id perhaps a modal on the same page even */}
+        <Card>
+          <CardHeader>
+            <CardTitle>Sample term</CardTitle>
+            <CardDescription></CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </CardContent>
+          <CardFooter>
+            <p>Difficulty 5</p>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
