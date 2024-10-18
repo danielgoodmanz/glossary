eda0a56bf7ae6e9a3fd4020aa104f84b4e20e329
Daniel Zayas <dan@Mac.attlocal.net>
2024-10-18 19:12:55 -0400

    dark mode functionality wip

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import Home from '../pages/Home';
 import Navbar from './my_components/Navbar';
 import Header from './my_components/Header';
+import { ThemeProvider } from '@/components/theme-provider';
 
 function App() {
   return (
