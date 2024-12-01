1c56f126629db15838390953732cd98a187bc5f4
Daniel Zayas <dan@Daniels-MacBook-Pro-2.local>
2024-11-30 22:01:00 -0500

    test commit

--- src/my_components/Header.jsx
+++ src/my_components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <h2 className='font-extrabold text-4xl tracking-tight text-center my-4'>
-      Saida/Jermaines' Deal Menu
+      Saida & Jermaines' Deal Menu
     </h2>
   );
 };
